e399777fd2aaccf5777659c47ae895d1f54a5161
Vijay S Kumar <63061945+vijays-hub@users.noreply.github.com>
2020-12-05 19:59:29 +0530

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAajfjWMvkeQOmcP0wrG89y3wSiEkph6Hw",
-    authDomain: "clone-c32e4.firebaseapp.com",
-    databaseURL: "https://clone-c32e4.firebaseio.com",
-    projectId: "clone-c32e4",
-    storageBucket: "clone-c32e4.appspot.com",
-    messagingSenderId: "337586569218",
-    appId: "1:337586569218:web:cfb6cf2a9276456b41671b",
-    measurementId: "G-5E4HCLRGGH"
+//    Your Firebase Config Object Goes Here....
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
